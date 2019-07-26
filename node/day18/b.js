46d6d36998bbf0656f9ce8e234c93236472e13f1
yestodorrow <power998@163.com>
2019-07-26 18:02:20 +0800

    promise

--- node/day18/b.js
+++ node/day18/b.js
@@ -1,8 +1,42 @@
-function hideString(str){
-    return str.replace(/[a-zA-Z]/g,"X")
+function hideString(str,done){
+    process.nextTick(()=>{
+        done(str.replace(/[a-zA-Z]/g,"X"))
+    })
 }
-var hidden=hideString("hello world");
+hideString("hello world",(hidden)=>{
+    console.log(hidden)
+})
 function log(str){
     console.log(str)
 }
-log(hidden)
+
+log("end")
+
+function delay(seconds,callback){
+    setTimeout(callback,seconds*1000)
+}
+log("starting dealys")
+delay(2,()=>{
+    console.log("two seconds");
+    delay(1,()=>{
+        log("3s");
+        delay(1,()=>{
+            log("4s")
+        })
+    })
+})
+var delay=(seconds)=>new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("the long dealy has ended")
+    },seconds*1000)
+});
+
+delay(1).then((msg)=>{
+    log("the dealy has ended");
+    log(msg)
+}).then(()=>{
+    console.log(3)
+})
+.then(()=>{
+    log(4)
+})
